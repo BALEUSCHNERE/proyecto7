1079a37950d1bb0c772b6579ff9e9c542c000589
BALEUSCHNERE <baleuschnere@gmail.com>
2025-03-12 01:17:55 -0600

    LE AGREGUE UN TITULO

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
-
+st.header('ESTE ES MI PRIMER APP WEB')
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
